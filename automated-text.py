f43fb3ea6a4289ae0f29b66a39eb595f656eee78
Dennis Idowu <dennis_idowu@live.com>
2020-10-30 23:53:18 -0400

    imported twilio

--- automated-text.py
+++ automated-text.py
@@ -1,8 +1,17 @@
 """Step 1: [Text Messages] have a collection/array of preset messages sent via my_messages = [" ", ""]"""
 
+from twilio.rest import Client
 GOOD_MORNING_QUOTES = [
     "Good Morning Love!",
     "How are you doing today?"
 ]
 
 """Step: 2: [Send to S/O] Send Preset Message Using API send_message(my_messages[0])"""
+
+"""Creation of function for the twilio API"""
+
+
+def send_message(quote):
+    account = ""
+    token = ""
+    client = Client(account, token)
