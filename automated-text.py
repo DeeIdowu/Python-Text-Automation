88d4bdda8ef64e5c7845f45beae90d5d1a543dc9
Dennis Idowu <dennis_idowu@live.com>
2020-10-30 23:47:57 -0400

    first commit

---
+++ automated-text.py
@@ -0,0 +1,8 @@
+"""Step 1: [Text Messages] have a collection/array of preset messages sent via my_messages = [" ", ""]"""
+
+GOOD_MORNING_QUOTES = [
+    "Good Morning Love!",
+    "How are you doing today?"
+]
+
+"""Step: 2: [Send to S/O] Send Preset Message Using API send_message(my_messages[0])"""
