97a64779953bf667b9e976a2456f349c413a53a7
Dennis Idowu <dennis_idowu@live.com>
2020-11-02 18:26:10 -0500

    Added account number and token from twilio api

--- automated-text.py
+++ automated-text.py
@@ -15,8 +15,8 @@ twilio_number = 234
 
 
 def send_message(quote):
-    account = ""
-    token = ""
+    account = "AC011b8f39d3022b8b0ce98de3afb723f3"
+    token = "41558565abcd668d361b4eb494ee679f"
     client = Client(account, token)
 
     client.messages.create(to=cellphone,
